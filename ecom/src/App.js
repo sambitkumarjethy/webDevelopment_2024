ddd6d452144c1097cde8a0fdcfe68e223f7e566b
sambitkumarjethy <sambitkumarjethy@gmail.com>
2024-07-25 09:00:09 +0530

    ecom- produt list page setup

---
+++ ecom/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import ProductList from "./features/product-list/Product-list";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ ecom/src/features/counter/counterAPI.js
@@ -0,0 +1,26 @@
+// // A mock function to mimic making an async request for data
+// export function fetchCount(amount = 1) {
+//   return new Promise((resolve) =>{
+//     // setTimeout(() => resolve({ data: amount }), 500)
+//    const response =  await fetch ('http://localhost:8080')
+//    const result = await response.json()
+//    resolve({data})
+//   }
+
+//   );
+// }
+
+export async function fetchCount(amount = 1) {
+  return new Promise(async (resolve) => {
+    // Mocking a delay for async behavior
+    // setTimeout(() => resolve({ data: amount }), 500);
+
+    try {
+      const response = await fetch("http://localhost:8080");
+      const result = await response.json();
+      resolve({ data: result });
+    } catch (error) {
+      resolve({ data: null, error });
+    }
+  });
+}
