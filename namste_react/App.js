16a2988bb673d224a9e6f4e4132926e362eb5419
sambitkumarjethy <sambitkumarjethy@gmail.com>
2024-08-16 15:47:21 +0530

    namste react - laying faundation use us jsx and nested jsx

--- namste_react/App.js
+++ namste_react/App.js
@@ -42,7 +42,31 @@ const sibling = React.createElement("div", { id: "parrent" }, [
     React.createElement("h2", {}, "I Am h2 as a sibling component !"),
   ]),
 ]);
+
 console.log(sibling);
+
+const jsx_heading = (
+  <h1 className="head" id="jsx_heading">
+    This is Jsx heading element
+  </h1>
+);
+const Tittle = function () {
+  return <h1>This is tittle component</h1>;
+};
+// React function component
+// component composition - binding  multiple component in a single component
+const HeadingComponent = () => {
+  return (
+    <div>
+      {jsx_heading}
+      <jsx_heading />
+      <Tittle />
+      <h1 className="heading">This is heading component !</h1>
+    </div>
+  );
+};
+console.log(jsx_heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(sibling);
+// root.render(`jsx_heading`); // rendering a element
+root.render(<HeadingComponent />); // rendering a component
