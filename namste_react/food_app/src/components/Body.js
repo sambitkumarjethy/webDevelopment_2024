dbca0f4704fb2bb1487fbce098c72efd7ef467f0
sambitkumarjethy <sambitkumarjethy@gmail.com>
2024-08-25 10:49:03 +0530

    namste react - Restorat rating filter added

--- namste_react/food_app/src/components/Body.js
+++ namste_react/food_app/src/components/Body.js
@@ -1,12 +1,27 @@
 import RestroCard from "./RestroCard";
 import resList from "../utils/mockdata";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > "4"
+            );
+            setListOfRestaurants(filteredList);
+            console.log(listOfRestaurants);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestroCard resData={restaurant} key={restaurant.info.id} />
         ))}
       </div>
