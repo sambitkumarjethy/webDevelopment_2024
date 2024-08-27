47e5234563da194d79fa0f32e69e50a11667655c
sambitkumarjethy <sambitkumarjethy@gmail.com>
2024-08-27 10:43:37 +0530

    namste react - swiggy API integration and shimmer UI added

--- namste_react/food_app/src/components/Body.js
+++ namste_react/food_app/src/components/Body.js
@@ -1,9 +1,32 @@
 import RestroCard from "./RestroCard";
-import resList from "../utils/mockdata";
-import { useState } from "react";
+// import resList from "../utils/mockdata";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(
+      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setListOfRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
